1507f2cd16c5f1f190400262f85cd28522fc84ce
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-03-08 00:37:43 +0200

    modification topBar & imgSlider

--- app/src/main/java/com/example/scooby/scooby/network/PlogApi.kt
+++ app/src/main/java/com/example/scooby/scooby/network/PlogApi.kt
@@ -1,6 +1,15 @@
 package com.example.scooby.scooby.network
 
+import com.example.scooby.scooby.data.model.BlogResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
 
 interface BlogApi {
+    @GET("/scooby/api/Plogs/getallplogs")
+    suspend fun getBlog() : Response<List<BlogResponse>>
 
+    companion object{
+
+    }
 }
--- app/src/main/java/com/example/scooby/scooby/ui/HomeFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/HomeFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.denzcoskun.imageslider.constants.AnimationTypes
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
+import com.example.scooby.R
 import com.example.scooby.utils.BaseResponse
 import com.example.scooby.databinding.FragmentHomeBinding
 import com.example.scooby.scooby.adapter.ServicesAdapter
@@ -82,10 +83,11 @@ class HomeFragment : Fragment() {
 
     private fun discount() {
         val imgList = ArrayList<SlideModel>()
-        imgList.add(SlideModel("https://s3-alpha-sig.figma.com/img/51e8/e3e8/003e55131e1fe3c1c2c03a948d92d5a7?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Oiop4EyMQ2hAISmB4cvrFFkPbjNWNqqBCpXVR0pEFS7pZOaHUYrsX8jvjRcGJTKMK55xEzJlCkZrFkx4fNiohEDJlDQeNk6krDV3-AFBEalLswW1pBVjoSTKQKdsRQij~jKovYaQEOa-aWXonJDvV0h~4Sjx4tWkCkJx0-gvN~sevfdpCdO5Do9zaxkDGwsrG41LcPkLDPVza~VlavM79OzhmQegZT3edwHpJF-6b-LQp8KkFhuAtFzlK8mB2x3k4DqGhaIGDjQqTTfeJfrpRBFgyaEhQni7mcM5n1eeN-cACs8efdXmKVSdlNNWAgf6RmQLY3pD7RZieuRBCUofOQ__"))
+        imgList.add(SlideModel(R.drawable.item_plogs_1))
+//        imgList.add(SlideModel("https://s3-alpha-sig.figma.com/img/51e8/e3e8/003e55131e1fe3c1c2c03a948d92d5a7?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Oiop4EyMQ2hAISmB4cvrFFkPbjNWNqqBCpXVR0pEFS7pZOaHUYrsX8jvjRcGJTKMK55xEzJlCkZrFkx4fNiohEDJlDQeNk6krDV3-AFBEalLswW1pBVjoSTKQKdsRQij~jKovYaQEOa-aWXonJDvV0h~4Sjx4tWkCkJx0-gvN~sevfdpCdO5Do9zaxkDGwsrG41LcPkLDPVza~VlavM79OzhmQegZT3edwHpJF-6b-LQp8KkFhuAtFzlK8mB2x3k4DqGhaIGDjQqTTfeJfrpRBFgyaEhQni7mcM5n1eeN-cACs8efdXmKVSdlNNWAgf6RmQLY3pD7RZieuRBCUofOQ__"))
         imgList.add(SlideModel("https://s3-alpha-sig.figma.com/img/b942/f329/7458e2f3c8eaa5bbb87439b0b51feee3?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZJNPYgfkpEXP5tjeVW~OO4qywsj34xbcSe4fh2YRwWPvXTg9kJ1~0iMjG2U3at1SEfUkr8gEu068h-cdL3qFCJCpnwmWOw6w47ikDunhedg783-LvBiF5E~k2QiSa5CtKvvAYamk2SenJRFoIS9mP8Lr63v9oxg~XpRCoA8F89iHcc3nJL67qWaw8cENdU6slfuoQUqphpU2yOri5aD3es6L5aLeD19wGAo7yhuK-h5VKZ23EZMBtSdU8BcT-Kc5vlxehmbw8Qppp7K3xaeL5XVDtn5En-vWLiR~Wp6az53eVf4ucA3hE8tfKJWMCjylN71~ndo215A6YAvjxGaPxA__"))
         imgList.add(SlideModel("https://s3-alpha-sig.figma.com/img/2793/085f/6103dac34bb37e214c315326a726b1a1?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UW7gpMi~epi6nmM7WYBAYULyg7b5pZ4~WFVP0E36j1Vn5avN7LxOd074uhfjAEfB0delHibUhr1X7MxEpFxCp6uSfJsEyalDWAY8xBdtZFNEJtSKyu0nnZQm7pIKK5~gkgrqMI3EWeGv570aekqTFUywKZzQ8V9oTgGAkfKhE61uepWOXZMlP3weXwc3t99Zq8mG0So6lB0a8eayqu55JCgljWUtwLc3M2UQx8vAY~tFl57aIJ47XkQrl1If--id5WS90ck7e-IgotKAR6mkW1Ux6slfSmvEJUeArdNx-kK1ZLdajRcjQF1jYQPbp0-fSwfh5DwkZLF1n802Wm3tJw__"))
-        binding.imageSlider.setImageList(imgList,ScaleTypes.FIT)
+        binding.imageSlider.setImageList(imgList,ScaleTypes.CENTER_CROP)
         binding.imageSlider.setSlideAnimation(AnimationTypes.DEPTH_SLIDE)
     }
 }
