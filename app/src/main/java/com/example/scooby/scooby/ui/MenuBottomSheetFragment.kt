ea7d90a6b710ab2ee7d3525fe51e4998369331ca
Mohamed Ashraf <ma1261327@gmail.com>
2024-03-13 05:07:15 +0200

    menu bottom sheet update

--- app/src/main/java/com/example/scooby/scooby/ui/MenuBottomSheetFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/MenuBottomSheetFragment.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentHomeBinding
+import com.example.scooby.databinding.MenuBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MenuBottomSheetFragment : BottomSheetDialogFragment() {
 
-
+    private var _binding: MenuBottomSheetBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.menu_bottom_sheet, container, false)
+        _binding = MenuBottomSheetBinding.inflate(inflater)
+
+        binding.vetIcon.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_vetFragment)
+        }
+        return binding.root
     }
 
 
