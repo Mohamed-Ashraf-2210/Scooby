3d94e7c9805374ded2a971cd36fbcb3b9e85590b
Mohamed Ashraf <ma1261327@gmail.com>
2024-03-21 20:42:16 +0200

    Crash when select image from gallery

--- app/src/main/java/com/example/scooby/scooby/ui/EditProfileFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/EditProfileFragment.kt
@@ -1,6 +1,12 @@
 package com.example.scooby.scooby.ui
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,39 +15,92 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Glide.init
 import com.example.scooby.MainActivity
-import com.example.scooby.R
 import com.example.scooby.databinding.FragmentEditProfileBinding
 import com.example.scooby.scooby.data.model.ProfileDetailsResponse
 import com.example.scooby.scooby.viewmodel.ProfileViewModel
 import com.example.scooby.utils.Constant
 import com.example.scooby.utils.TokenManager
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.io.FileOutputStream
 
 
 class EditProfileFragment : Fragment() {
     private lateinit var binding: FragmentEditProfileBinding
     private val profileViewModel by viewModels<ProfileViewModel>()
     private lateinit var userId: String
-
+    private val REQUEST_IMAGE_PICKER = 101
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentEditProfileBinding.inflate(inflater)
         init()
+
+        binding.editImageProfile.setOnClickListener {
+            pickImage()
+        }
         return binding.root
     }
 
+    private fun pickImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_IMAGE_PICKER)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_PICKER && resultCode == Activity.RESULT_OK) {
+            val imageUri = data?.data
+            if (imageUri != null) {
+                val file = saveImageToFile(imageUri)
+//                val requestFile: RequestBody = file.asRequestBody("image/*".toMediaTypeOrNull())
+//                val profileImagePart = MultipartBody.Part.createFormData("profileImage", file.name, requestFile)
+                profileViewModel.apply {
+                    uploadImage(userId,file)
+                    profileResult.observe(viewLifecycleOwner) {
+                        getUserData()
+                    }
+                }
+            } else {
+                Log.e(Constant.MY_TAG, "imageUri is Null")
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun saveImageToFile(imageUri: Uri): File {
+        val inputStream = imageUri.let { requireContext().contentResolver.openInputStream(it) }
+        val file = File(context?.cacheDir, "selected_image.png")
+        val outputStream = FileOutputStream(file)
+
+        inputStream?.use { input ->
+            outputStream.use { output ->
+                input.copyTo(output)
+            }
+        }
+        return file
+    }
+
+
     private fun init() {
         userId = TokenManager.getAuth(requireContext(), Constant.USER_ID).toString()
+        getUserData()
+        backOffFragment()
+    }
+
+    private fun getUserData() {
         profileViewModel.apply {
             getUser(userId)
             profileResult.observe(viewLifecycleOwner) {
                 getProfileData(it)
             }
         }
-        backOffFragment()
     }
 
     private fun getProfileData(it: ProfileDetailsResponse?) {
--- app/src/main/java/com/example/scooby/scooby/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/scooby/scooby/viewmodel/ProfileViewModel.kt
@@ -6,12 +6,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.scooby.scooby.data.model.ProfileDetailsResponse
+import com.example.scooby.scooby.data.model.UpdateUseResponse
 import com.example.scooby.scooby.repository.ProfileRepo
 import com.example.scooby.utils.Constant
 import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import java.io.File
 
 class ProfileViewModel: ViewModel() {
     private val profileRepo = ProfileRepo()
+
+    // region Get user
     private val _profileResult: MutableLiveData<ProfileDetailsResponse?> = MutableLiveData()
     val profileResult: LiveData<ProfileDetailsResponse?>
         get() = _profileResult
@@ -27,6 +32,25 @@ class ProfileViewModel: ViewModel() {
                 Log.e(Constant.MY_TAG, "ERROR FETCHING URLS $e")
             }
 
+        }
+    }
+    // endregion
+
+
+    private val _uploadImageResult: MutableLiveData<UpdateUseResponse?> = MutableLiveData()
+    val uploadImageResult: LiveData<UpdateUseResponse?>
+        get() = _uploadImageResult
+
+    fun uploadImage(userId: String, profileImagePart: File) {
+        viewModelScope.launch {
+            try {
+                val response = profileRepo.uploadImageProfile(userId,profileImagePart)
+                if (response != null && response.isSuccessful) {
+                    _uploadImageResult.value = response.body()
+                }
+            } catch (e: Exception) {
+                Log.e(Constant.MY_TAG, "ERROR FETCHING URLS $e")
+            }
         }
     }
 }
