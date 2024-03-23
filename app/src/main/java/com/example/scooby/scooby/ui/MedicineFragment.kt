6c158dee6d3fab489462dfa2c96c28b73cc13314
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-03-23 09:19:04 +0200

    finish item medicine

--- app/src/main/java/com/example/scooby/scooby/ui/MedicineFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/MedicineFragment.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentMedicineBinding
+import com.example.scooby.databinding.FragmentServiceBinding
 
 class MedicineFragment : Fragment() {
 
-
+    private var _binding: FragmentMedicineBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medicine, container, false)
+        _binding = FragmentMedicineBinding.inflate(layoutInflater,container,false)
+        makeStrikeOnText()
+        return binding.root
+    }
+
+    private fun makeStrikeOnText() {
     }
 
 }
