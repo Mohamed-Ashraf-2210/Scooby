6e236940dbf94e7fb642b2472070426e36aa5d1c
Mo-AShraaf <74676105+Mohamed-Ashraf-2210@users.noreply.github.com>
2024-04-01 00:27:33 +0200

    Update MenuBottomSheetFragment.kt

--- app/src/main/java/com/example/scooby/scooby/ui/MenuBottomSheetFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/MenuBottomSheetFragment.kt
@@ -4,22 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.scooby.R
 import com.example.scooby.databinding.MenuBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MenuBottomSheetFragment : BottomSheetDialogFragment() {
 
-    private var _binding: MenuBottomSheetBinding? = null
-    private val binding get() = _binding!!
+    private var binding: MenuBottomSheetBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = MenuBottomSheetBinding.inflate(inflater)
+    ): View? {
+        binding = MenuBottomSheetBinding.inflate(inflater)
 
-        return binding.root
+        binding?.apply {
+            sittingIcon.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_vetFragment)
+            }
+        }
+        return binding?.root
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 
 }
