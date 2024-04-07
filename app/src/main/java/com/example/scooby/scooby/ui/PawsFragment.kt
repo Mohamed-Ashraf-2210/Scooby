94ee4de31e8ae0276564588c6734b98b868b42c8
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-04-07 09:14:21 +0200

    work on paws layout and finish AdaptionFragment layout

--- app/src/main/java/com/example/scooby/scooby/ui/PawsFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/PawsFragment.kt
@@ -6,16 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentPawsBinding
 
 
 class PawsFragment : Fragment() {
+    private val adaptionFragment = AdaptionFragment()
+    private val rescueFragment = RescueFragment()
+    private val missingFragment = MissingFragment()
+    private lateinit var binding : FragmentPawsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_paws, container, false)
+        binding = FragmentPawsBinding.inflate(inflater,container,false)
+        initButtonCallBack()
+        return binding.root
+    }
+
+    private fun initButtonCallBack() {
+        binding.btnAdaption.setOnClickListener {
+            replaceFragment(adaptionFragment)
+        }
+        binding.btnRescue.setOnClickListener {
+            replaceFragment(rescueFragment)
+        }
+        binding.btnMissing.setOnClickListener {
+            replaceFragment(missingFragment)
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+         childFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container_paws,fragment)
+            commit()
+         }
     }
 
 }
