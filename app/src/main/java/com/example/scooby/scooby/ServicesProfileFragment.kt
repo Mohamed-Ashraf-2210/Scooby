36d4f5157c007fab4ec0f560541081a7453b11ca
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-04-22 08:31:38 +0200

    working on Services Profile layout

---
+++ app/src/main/java/com/example/scooby/scooby/ServicesProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.example.scooby.scooby
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.scooby.R
+
+class ServicesProfileFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_services_profile, container, false)
+    }
+
+}
