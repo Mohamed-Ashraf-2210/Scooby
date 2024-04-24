031a94c0a2bb3a0e08e936adc8ead7920a0add85
Mohamed Ashraf <ma1261327@gmail.com>
2024-04-25 00:08:10 +0200

    switch in community

--- app/src/main/java/com/example/scooby/scooby/ui/CommunityFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ui/CommunityFragment.kt
@@ -1,21 +1,38 @@
 package com.example.scooby.scooby.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentCommunityBinding
 
 
 class CommunityFragment : Fragment() {
+    private var binding: FragmentCommunityBinding? = null
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        binding = FragmentCommunityBinding.inflate(inflater, container, false)
+        binding?.apply {
+            switchOnOff.setOnCheckedChangeListener { _, checked ->
+                when {
+                    checked -> {
+                        publicBtn.setTextColor(ContextCompat.getColor(requireContext(), R.color.primary))
+                        myMomentsBtn.setTextColor(ContextCompat.getColor(requireContext(),R.color.white))
+                    }
+                    else -> {
+                        publicBtn.setTextColor(ContextCompat.getColor(requireContext(),R.color.white))
+                        myMomentsBtn.setTextColor(ContextCompat.getColor(requireContext(),R.color.primary))
+                    }
+                }
+            }
+        }
+        return binding?.root
     }
 }
