91d689da8b3642b5b374360741493badc5f04684
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-04-25 09:21:31 +0200

    add favorite and un favorite paws fragment done

--- app/src/main/java/com/example/scooby/scooby/paws/adapter/AdaptionAdoptMeAdapter.kt
+++ app/src/main/java/com/example/scooby/scooby/paws/adapter/AdaptionAdoptMeAdapter.kt
@@ -2,17 +2,44 @@ package com.example.scooby.scooby.paws.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.domain.paws.AdaptionAdoptMeResponse
 import com.example.scooby.databinding.ItemAdaptionAdoptBinding
+import com.example.scooby.scooby.paws.viewmodel.PawsViewModel
 import com.example.scooby.utils.loadUrl
+import com.varunest.sparkbutton.SparkEventListener
 
-class AdaptionAdoptMeAdapter(private val listOfData: AdaptionAdoptMeResponse) : RecyclerView.Adapter<AdaptionAdoptMeAdapter.AdaptionAdoptMeViewHolder>() {
+class AdaptionAdoptMeAdapter(private val listOfData: AdaptionAdoptMeResponse,val userId:String?,val pawsViewModel: PawsViewModel) : RecyclerView.Adapter<AdaptionAdoptMeAdapter.AdaptionAdoptMeViewHolder>() {
     inner class AdaptionAdoptMeViewHolder(private val itemAdaptionAdoptBinding: ItemAdaptionAdoptBinding) :
         RecyclerView.ViewHolder(itemAdaptionAdoptBinding.root) {
         fun bind(item : AdaptionAdoptMeResponse.Data){
             item.petImage?.let {itemAdaptionAdoptBinding.imgItem.loadUrl(it)}
+            itemAdaptionAdoptBinding.petBio.text = item.profileBio
             itemAdaptionAdoptBinding.nameItem.text = item.name
+            setFavoritePet(item)
+        }
+
+        private fun setFavoritePet(itemPet: AdaptionAdoptMeResponse.Data) {
+            itemAdaptionAdoptBinding.heartIconId.setEventListener(object : SparkEventListener {
+                override fun onEvent(button: ImageView, buttonState: Boolean) {
+                    buttonState.apply {
+                        itemPet.id?.let {
+                            if (userId != null) {
+                                pawsViewModel.addPetToFavorite(userId, it)
+                            }
+                        }
+                    }
+
+                }
+
+                override fun onEventAnimationEnd(button: ImageView?, buttonState: Boolean) {
+
+                }
+                override fun onEventAnimationStart(button: ImageView?, buttonState: Boolean) {
+
+                }
+            })
         }
 
     }
