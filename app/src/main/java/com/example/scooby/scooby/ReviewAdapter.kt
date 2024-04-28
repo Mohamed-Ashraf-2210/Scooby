4ebefce6e271f459c871cc3644c818c0fffd5635
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-04-28 07:05:06 +0300

    finish ServicesProfile Part of Bind data with View & adapter ReviewAdapter

---
+++ app/src/main/java/com/example/scooby/scooby/ReviewAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.scooby.scooby
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.ServicesProfileResponse
+import com.example.scooby.databinding.ItemReviewBinding
+import com.example.scooby.utils.loadUrl
+
+class ReviewAdapter(
+    private val reviews: List<ServicesProfileResponse.Data.ReviewsOfService>
+) : RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>() {
+    lateinit var allReviews: ServicesProfileResponse.Data.ReviewsOfService
+
+    inner class ReviewViewHolder(private val itemReviewBinding: ItemReviewBinding) :
+        RecyclerView.ViewHolder(itemReviewBinding.root) {
+        fun bind(item: ServicesProfileResponse.Data.ReviewsOfService) {
+            item.user?.profileImage?.let { itemReviewBinding.civProfile.loadUrl(it) }
+            itemReviewBinding.tvName.text = item.name.toString()
+            itemReviewBinding.rate.rating = item.rating?.toFloat()!!
+            itemReviewBinding.tvReview.text = item.review.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        return ReviewViewHolder(
+            ItemReviewBinding.inflate(LayoutInflater
+                .from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = reviews.size
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        holder.bind(reviews[position])
+    }
+}
--- app/src/main/java/com/example/scooby/scooby/ServicesProfileFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/ServicesProfileFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.domain.ServicesProfileResponse
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentServicesProfileBinding
+import com.example.scooby.utils.loadUrl
 
 class ServicesProfileFragment : Fragment() {
-
+    private lateinit var binding: FragmentServicesProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,41 @@ class ServicesProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services_profile, container, false)
+        binding = FragmentServicesProfileBinding.inflate(layoutInflater,container,false)
+
+        return binding.root
+    }
+
+    fun setDataToViews(servicesDetails : ServicesProfileResponse){
+        binding.apply {
+            servicesDetails.updatedDoc?.imagesProfile?.get(0)
+                ?.let { servicesProfileImage.loadUrl(it) }
+            servicesName.text = servicesDetails.updatedDoc?.name ?: getString(R.string.error_loading)
+            servicesPrice.text = servicesDetails.updatedDoc?.price.toString()
+            servicesTime.text = servicesDetails.updatedDoc?.pricePer
+            rate.rating = servicesDetails.updatedDoc?.rate?.toFloat()!!
+            rateNumber.text = servicesDetails.updatedDoc?.numberOfRate.toString()
+            tvAboutDescription.text = servicesDetails.updatedDoc?.description
+            servicesDetails.updatedDoc?.question1?.get(0).let {
+                tvQuestion1.text = it
+            }
+            servicesDetails.updatedDoc?.question1?.get(1).let {
+                tvAnswar1.text = it
+            }
+            servicesDetails.updatedDoc?.question2?.get(0).let {
+                tvQuestion2.text = it
+            }
+            servicesDetails.updatedDoc?.question2?.get(1).let {
+                tvAnswar2.text = it
+            }
+            servicesDetails.updatedDoc?.question3?.get(0).let {
+                tvQuestion3.text = it
+            }
+            servicesDetails.updatedDoc?.question3?.get(1).let {
+                tvAnswar3.text = it
+            }
+
+        }
     }
 
 }
