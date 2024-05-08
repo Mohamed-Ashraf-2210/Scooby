6bda1994fa6fe0eed20a3d6baf3537a4782af73f
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-05-08 13:30:00 +0300

    ..

--- app/src/main/java/com/example/scooby/scooby/services/fragment/ServiceFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/services/fragment/ServiceFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.domain.services.ServicesResponse
+import com.example.scooby.R
 import com.example.scooby.databinding.FragmentServiceBinding
 import com.example.scooby.scooby.services.adapter.ServicesMainAdapter
 import com.example.scooby.scooby.services.viewmodel.ServicesViewModel
