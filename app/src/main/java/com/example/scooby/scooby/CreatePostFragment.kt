4c26d0fd85c3055cf02cc3bd7e1cf05cb17656dc
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-06-19 23:20:17 +0300

    add viewbinding to CreatePostFragment

--- app/src/main/java/com/example/scooby/scooby/CreatePostFragment.kt
+++ app/src/main/java/com/example/scooby/scooby/CreatePostFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentCreatePostBinding
 
 class CreatePostFragment : Fragment() {
+    private var binding : FragmentCreatePostBinding?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,10 +18,9 @@ class CreatePostFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_post, container, false)
+    ): View {
+        binding = FragmentCreatePostBinding.inflate(inflater,container,false)
+        return binding!!.root
     }
 
 }
