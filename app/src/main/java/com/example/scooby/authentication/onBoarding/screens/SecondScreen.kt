62093b7829f69a9e86b32be3d2d11671a946ebb6
Hossam Ahmed <74689537+HussamEraqi@users.noreply.github.com>
2024-07-02 02:38:03 +0300

    onBoarding ..

--- app/src/main/java/com/example/scooby/authentication/onBoarding/screens/SecondScreen.kt
+++ app/src/main/java/com/example/scooby/authentication/onBoarding/screens/SecondScreen.kt
@@ -5,56 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentFirstScreenBinding
+import com.example.scooby.databinding.FragmentSecondScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentSecondScreenBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_screen, container, false)
+        _binding = FragmentSecondScreenBinding.inflate(layoutInflater,container,false)
+
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+        binding.next2.setOnClickListener {
+            viewPager?.currentItem = 2
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- app/src/main/java/com/example/scooby/authentication/onBoarding/screens/ThirdScreen.kt
+++ app/src/main/java/com/example/scooby/authentication/onBoarding/screens/ThirdScreen.kt
@@ -1,60 +1,38 @@
 package com.example.scooby.authentication.onBoarding.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.scooby.R
+import com.example.scooby.databinding.FragmentSecondScreenBinding
+import com.example.scooby.databinding.FragmentThirdScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ThirdScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ThirdScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentThirdScreenBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_screen, container, false)
+        _binding = FragmentThirdScreenBinding.inflate(layoutInflater,container,false)
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+        binding.finish.setOnClickListener {
+            //nav to auth || home
+            onBoardingFinish()
+        }
+        return binding.root
+    }
+    private fun onBoardingFinish(){
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished",true)
+        editor.apply()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ThirdScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ThirdScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
